40d114a904d1a8eb3d1568f625dcfd1a6341288d
thdihan <thdihan@outlook.com>
2025-06-08 20:03:43 +0600

    Basic Redux setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SessionProvider } from "next-auth/react";
 import Session from "@/components/providers/Session";
+import StoreProvider from "@/components/providers/StoreProvider";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -29,8 +30,9 @@ export default function RootLayout({
             <body
                 className={`${geistSans.variable} ${geistMono.variable} antialiased`}
             >
-                <Session>{children}</Session>
-                {/* {children} */}
+                <StoreProvider>
+                    <Session>{children}</Session>
+                </StoreProvider>
             </body>
         </html>
     );
